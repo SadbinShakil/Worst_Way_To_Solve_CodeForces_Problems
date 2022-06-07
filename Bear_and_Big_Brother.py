807cd83475c14a81689ce3b5d9dd7d32dcde86a1
Sakil Sarker <61023087+SadbinShakil@users.noreply.github.com>
2022-06-07 15:42:49 +0600

    Add files via upload

---
+++ Bear_and_Big_Brother.py
@@ -0,0 +1,16 @@
+Limarks_weight = int(input("Enter the Limark's Weight: "))
+Bobs_weight = int(input("Enter the Bob's Weight: "))
+
+if(Bobs_weight>=Bobs_weight):
+    l = [1]
+    for x in l:
+        l.append(x + 1)
+        Limarks_weight = Limarks_weight * 3
+        Bobs_weight = Bobs_weight * 2
+        if(Limarks_weight > Bobs_weight):
+            break
+else:
+    print("Condition Violation")
+
+
+print("Total years needed: ",x)
